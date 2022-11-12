ca1abd9e6d5e87ea57f431e1c1349b1c12f46267
Yordan Yordanov <danchhoo@gmail.com>
2022-11-12 22:15:55 +0200

    Creating ArchitectureEntity for the exterior projects

---
+++ DesignHelper.Infrastructure/Constrains/ConstrainValidations.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignHelper.Infrastructure.Constrains
+{
+    public class ConstrainValidations
+    {
+        //Title
+        public const int TitleMinLength = 5;
+        public const int TitleMaxLength = 50;
+        
+        //Desctription
+        public const int DescriptionMinLength = 5;
+        public const int DescriptionMaxLength = 1000;
+    }
+}
--- DesignHelper.Infrastructure/Data/ApplicationDbContext.cs
+++ DesignHelper.Infrastructure/Data/ApplicationDbContext.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
-namespace DesignHelper.Data
+namespace DesignHelper.Infrastructure.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
